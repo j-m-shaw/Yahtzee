17a9d972faba98766941e5e8905c0285086b7b88
unknown <shaw.150@wright.edu>
2018-10-18 13:25:54 -0400

    create base file for test harness

---
+++ testHarness.py
@@ -0,0 +1,10 @@
+#method to clear score card to allow for tests of same spot on the score 
+#card to be run one after the other
+
+#iterate through the list of cases in the test file for top scores
+
+#iterate through list of scores to check if added correctly
+
+#iterate through list of scores to check if bonus should be added
+
+#iterate through scores to check if bonus is added correctly.
